3934735de1140758f3e4697a2326f1d5376be404
Sanjiv1458 <sanjiv1458@gmail.com>
2023-12-06 00:20:45 +0530

    #

--- config/email.js
+++ config/email.js
@@ -2,20 +2,14 @@ import nodemailer from 'nodemailer';
 
 let transporter;
 
-try {
-  transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  console.log('Nodemailer transporter created successfully');
-} catch (error) {
-  console.error('Error creating Nodemailer transporter:', error.message);
-}
+transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 export default transporter;
